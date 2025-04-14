41a1ba31d034603a50523739bb6e945a1a1c711f
Vedant-Shrirao <vedu.shriao07@gmail.com>
2025-04-14 16:03:38 +0530

    message

--- lib/main.dart
+++ lib/main.dart
@@ -1,365 +1,10 @@
-// // // import 'package:flutter/material.dart';
-// // //
-// // // void main() {
-// // //   runApp(LibraryApp());
-// // // }
-// // //
-// // // class LibraryApp extends StatelessWidget {
-// // //   @override
-// // //   Widget build(BuildContext context) {
-// // //     return MaterialApp(
-// // //       debugShowCheckedModeBanner: false,
-// // //       title: 'Library Management',
-// // //       theme: ThemeData(primarySwatch: Colors.blue),
-// // //       home: HomeScreen(),
-// // //     );
-// // //   }
-// // // }
-// // // class HomeScreen extends StatelessWidget {
-// // //   @override
-// // //   Widget build(BuildContext context) {
-// // //     return Scaffold(
-// // //       appBar: AppBar(title: Text("BOOKNEST")),
-// // //       drawer: Drawer(
-// // //         child: ListView(
-// // //           padding: EdgeInsets.zero,
-// // //           children: <Widget>[
-// // //             DrawerHeader(
-// // //               decoration: BoxDecoration(color: Colors.blue),
-// // //               child: Column(
-// // //                 children: [
-// // //                   Text('Library Menu', style: TextStyle(color: Colors.white, fontSize: 24)),
-// // //                   Row(
-// // //                     children: [
-// // //                       Container(
-// // //                         width: 50,
-// // //                         height: 50,
-// // //                         decoration: BoxDecoration(
-// // //                           shape: BoxShape.circle,
-// // //                           color: Colors.amberAccent,
-// // //                         ),
-// // //                       ),
-// // //                       Text('QWERTY')
-// // //                     ],
-// // //                   )
-// // //                 ],
-// // //               ),
-// // //             ),
-// // //             ListTile(
-// // //               title: Text('Home'),
-// // //               onTap: () => Navigator.pop(context),
-// // //             ),
-// // //             ListTile(
-// // //               title: Text('Profile'),
-// // //               onTap: () {},
-// // //             ),
-// // //             ListTile(
-// // //               title: Text('Books Issued'),
-// // //               onTap: () {},
-// // //             ),
-// // //             ListTile(
-// // //               title: Text('Tracker'),
-// // //               onTap: () {},
-// // //             ),
-// // //             ListTile(
-// // //               title: Text('Settings'),
-// // //               onTap: () {},
-// // //             ),
-// // //           ],
-// // //         ),
-// // //       ),
-// // //       body: Container(
-// // //         width: double.infinity,
-// // //         child: Column(
-// // //
-// // //           spacing: 20,
-// // //           mainAxisAlignment: MainAxisAlignment.center,
-// // //           crossAxisAlignment: CrossAxisAlignment.start,
-// // //           children: [
-// // //             Container(
-// // //                 width: 150,
-// // //                 height: 150,
-// // //                 decoration: BoxDecoration(
-// // //                   color: Colors.blue,
-// // //                   border: Border.all(color: Colors.black, width: 2),
-// // //                   borderRadius: BorderRadius.circular(10),
-// // //                 ),
-// // //               child: Center(
-// // //                 child: Text("data"),
-// // //               ),
-// // //               ),
-// // //             Container(
-// // //               width: 150,
-// // //               height: 150,
-// // //               decoration: BoxDecoration(
-// // //                 color: Colors.blue,
-// // //                 border: Border.all(color: Colors.black, width: 2),
-// // //                 borderRadius: BorderRadius.circular(10),
-// // //               ),
-// // //               child: Center(
-// // //                 child: Text("data"),
-// // //               ),
-// // //             ),
-// // //           ],
-// // //         ),
-// // //       ),
-// // //       );
-// // //   }
-// // // }
-// //
-// //
-//
-// import 'package:flutter/material.dart';
-// import 'package:badges/badges.dart' as badges;
-//
-// void main() {
-//   runApp(LibraryApp());
-// }
-//
-// class LibraryApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       title: 'Library Management',
-//       theme: ThemeData(primarySwatch: Colors.blue),
-//       home: HomeScreen(),
-//     );
-//   }
-// }
-//
-// class HomeScreen extends StatelessWidget {
-//   final List<String> boxLabels = [
-//     "Literature",
-//     "Computing",
-//     "Mathematics",
-//     "Science",
-//     "Fiction",
-//     "Economics"
-//   ];
-//   final int notificationCount = 5;
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text("BOOKNEST"),
-//         actions: [
-//           badges.Badge(
-//             badgeContent: Text(
-//               notificationCount.toString(),
-//               style: TextStyle(color: Colors.white),
-//             ),
-//             position: badges.BadgePosition.topEnd(top: 0, end: 3),
-//             child: IconButton(
-//               icon: Icon(Icons.notifications),
-//               onPressed: () {
-//                 Navigator.push(context,MaterialPageRoute(builder: (context)=>NotificationsPage()),
-//                 );
-//                 // Handle notification tap
-//               },
-//             ),
-//           ),
-//         ],
-//       ),
-//       drawer: Drawer(
-//         child: ListView(
-//           padding: EdgeInsets.zero,
-//           children: <Widget>[
-//             DrawerHeader(
-//               decoration: BoxDecoration(color: Colors.brown.shade300),
-//               child: Column(
-//                 children: [
-//                   Text(
-//                     'Library Menu',
-//                     style: TextStyle(color: Colors.white, fontSize: 24),
-//                   ),
-//                   Row(
-//                     children: [
-//                       Container(
-//                         width: 50,
-//                         height: 50,
-//                         decoration: BoxDecoration(
-//                           shape: BoxShape.circle,
-//                           color: Colors.amberAccent,
-//                         ),
-//                       ),
-//                       SizedBox(width: 10),
-//                       Text('QWERTY')
-//                     ],
-//                   )
-//                 ],
-//               ),
-//             ),
-//             ListTile(title: Text('Home'), onTap: () => Navigator.pop(context)),
-//             ListTile(title: Text('Profile'), onTap: () {}),
-//             ListTile(title: Text('Books Issued'), onTap: () {}),
-//             ListTile(title: Text('Tracker'), onTap: () {}),
-//             ListTile(title: Text('Settings'), onTap: () {}),
-//           ],
-//         ),
-//       ),
-//       body: Container(
-//         decoration: BoxDecoration(
-//           image: DecorationImage(
-//             image: NetworkImage(
-//                 "https://cdn.pixabay.com/photo/2017/08/06/22/01/books-2596809_1280.jpg"),
-//             fit: BoxFit.cover,
-//           ),
-//         ),
-//         child: Center(
-//           child: Padding(
-//             padding: const EdgeInsets.all(16.0),
-//             child: GridView.builder(
-//               shrinkWrap: true,
-//               gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-//                 crossAxisCount: 2,
-//                 crossAxisSpacing: 10,
-//                 mainAxisSpacing: 10,
-//                 childAspectRatio: 1,
-//               ),
-//               itemCount: boxLabels.length,
-//               itemBuilder: (context, index) {
-//                 return GestureDetector(
-//
-//                 onTap: () {
-//                   ScaffoldMessenger.of(context).showSnackBar(
-//                     SnackBar(content: Text("${boxLabels[index]} Clicked")),
-//                   );
-//                   Navigator.push(context,
-//                       MaterialPageRoute(builder: (context) => NotificationsPage()));
-//                 },
-//                 child:
-//                   Container(
-//                   width: 80,
-//                   height: 80,
-//                   decoration: BoxDecoration(
-//                     color: Colors.brown.shade300,
-//                     border: Border.all(color: Colors.black, width: 2),
-//                     borderRadius: BorderRadius.circular(10),
-//                   ),
-//                   child: Center(
-//                     child: Text(
-//                       boxLabels[index],
-//                       textAlign: TextAlign.center,
-//                       style: TextStyle(color: Colors.white, fontSize: 16),
-//                     ),
-//                   ),
-//                   ),
-//                 );
-//               },
-//             ),
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
-// //import 'package:flutter/material.dart';
-//
-// class NotificationsPage extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(title: Text("Notifications")),
-//       body: Center(
-//         child: Text(
-//           "No new notifications",
-//           style: TextStyle(fontSize: 18),
-//         ),
-//       ),
-//     );
-//   }
-// }
-//
-// class literature extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(title: Text("literature")),
-//       body: Center(
-//         child: Text(
-//           "No new notifications",
-//           style: TextStyle(fontSize: 18),
-//         ),
-//       ),
-//     );
-//   }
-// }
-// class computing extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(title: Text("computing")),
-//       body: Center(
-//         child: Text(
-//           "No new notifications",
-//           style: TextStyle(fontSize: 18),
-//         ),
-//       ),
-//     );
-//   }
-// }
-// class Mathematics extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(title: Text("mathematics")),
-//       body: Center(
-//         child: Text(
-//           "No new notifications",
-//           style: TextStyle(fontSize: 18),
-//         ),
-//       ),
-//     );
-//   }
-// }
-// class science extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(title: Text("science")),
-//       body: Center(
-//         child: Text(
-//           "No new notifications",
-//           style: TextStyle(fontSize: 18),
-//         ),
-//       ),
-//     );
-//   }
-// }
-// class Fiction extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(title: Text("Fiction")),
-//       body: Center(
-//         child: Text(
-//           "No new notifications",
-//           style: TextStyle(fontSize: 18),
-//         ),
-//       ),
-//     );
-//   }
-// }
-// class Economics extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(title: Text("Economics")),
-//       body: Center(
-//         child: Text(
-//           "No new notifications",
-//           style: TextStyle(fontSize: 18),
-//         ),
-//       ),
-//     );
-//   }
-// }
-
-
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 import 'package:badges/badges.dart' as badges;
+import 'package:shared_preferences/shared_preferences.dart';
+
+String apiUrl = "https://frmq1lm1-5000.inc1.devtunnels.ms";
 
 void main() {
   runApp(LibraryApp());
@@ -713,7 +358,67 @@ class Settings extends StatelessWidget {
     );
   }
 }
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController batchController = TextEditingController();
+  final TextEditingController phoneController = TextEditingController();
+
+  bool isLoading = false;
+
+  Future<void> registerUser() async {
+    final url = Uri.parse('${apiUrl}/api/users/register');
+
+    final payload = {
+      "name": nameController.text,
+      "email": emailController.text,
+      "password": passwordController.text,
+      "batch": batchController.text,
+      "phone": phoneController.text,
+    };
+
+    setState(() => isLoading = true);
+
+    try {
+      final response = await http.post(
+        url,
+        headers: {"Content-Type": "application/json"},
+        body: jsonEncode(payload),
+      );
+
+      final data = jsonDecode(response.body);
+      if (response.statusCode == 200 && data['token'] != null) {
+        // Store token in cache
+        final prefs = await SharedPreferences.getInstance();
+        await prefs.setString('auth_token', data['token']);
+
+        // Navigate to Home
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => HomeScreen()),
+        );
+      } else {
+        _showError(data['message'] ?? "Registration failed");
+      }
+    } catch (e) {
+      _showError("Error: $e");
+    } finally {
+      setState(() => isLoading = false);
+    }
+  }
+
+  void _showError(String message) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text(message), backgroundColor: Colors.red),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -721,62 +426,53 @@ class LoginPage extends StatelessWidget {
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(24.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(Icons.book, size: 100, color: Colors.brown),
-              SizedBox(height: 20),
-              Text("Welcome to BookNest",
-                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
-              SizedBox(height: 30),
-              TextField(
-                decoration: InputDecoration(
-                  labelText: "Username",
-                  border: OutlineInputBorder(),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(Icons.book, size: 100, color: Colors.brown),
+                SizedBox(height: 20),
+                Text("Welcome to BookNest",
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+                SizedBox(height: 30),
+                TextField(
+                  controller: nameController,
+                  decoration: InputDecoration(labelText: "Username", border: OutlineInputBorder()),
                 ),
-              ),
-              SizedBox(height: 15),
-              TextField(
-                decoration: InputDecoration(
-                  labelText: "email",
-                  border: OutlineInputBorder(),
+                SizedBox(height: 15),
+                TextField(
+                  controller: emailController,
+                  decoration: InputDecoration(labelText: "Email", border: OutlineInputBorder()),
                 ),
-              ),
-              SizedBox(height: 15),
-              TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  labelText: "Password",
-                  border: OutlineInputBorder(),
+                SizedBox(height: 15),
+                TextField(
+                  controller: passwordController,
+                  obscureText: true,
+                  decoration: InputDecoration(labelText: "Password", border: OutlineInputBorder()),
                 ),
-              ),
-              SizedBox(height: 15),
-              TextField(
-                decoration: InputDecoration(
-                  labelText: "batch",
-                  border: OutlineInputBorder(),
+                SizedBox(height: 15),
+                TextField(
+                  controller: batchController,
+                  decoration: InputDecoration(labelText: "Batch", border: OutlineInputBorder()),
                 ),
-              ),
-              SizedBox(height: 15),
-              TextField(
-                decoration: InputDecoration(
-                  labelText: "phone-number",
-                  border: OutlineInputBorder(),
+                SizedBox(height: 15),
+                TextField(
+                  controller: phoneController,
+                  decoration: InputDecoration(labelText: "Phone Number", border: OutlineInputBorder()),
                 ),
-              ),
-              SizedBox(height: 30),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(builder: (context) => HomeScreen()),
-                  );
-                },
-                child: Text("Login"),
-                style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.brown, foregroundColor: Colors.white),
-              ),
-            ],
+                SizedBox(height: 30),
+                ElevatedButton(
+                  onPressed: isLoading ? null : registerUser,
+                  child: isLoading
+                      ? CircularProgressIndicator(color: Colors.white)
+                      : Text("Login"),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.brown,
+                    foregroundColor: Colors.white,
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
